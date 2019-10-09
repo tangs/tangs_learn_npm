1e1b65511837c7f365019302f9754ab2b2ce1623
tangs <tangsong1988kaka@gmail.com>
2019-10-09 12:24:22 +0800

    update.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const lodash = require('lodash');
 
-const output = lodash.without([1, 2, 3], 3);
-console.log(output);
-
 exports.printMsg = () => {
+    const output = lodash.without([1, 2, 3], 3);
+    console.log(output);
     console.log('This is my first npm package.')
 }
